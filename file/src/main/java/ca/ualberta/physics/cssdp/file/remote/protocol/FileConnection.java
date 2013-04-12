75f8a61e5f65625d08ee956816e23257a0b2c228
Roddi Potter <potter.rod@gmail.com>
2013-04-11 23:25:23 -0600

    added license info, removed unused files

--- file/src/main/java/ca/ualberta/physics/cssdp/file/remote/protocol/FileConnection.java
+++ file/src/main/java/ca/ualberta/physics/cssdp/file/remote/protocol/FileConnection.java
@@ -1,3 +1,21 @@
+/* ============================================================
+ * FileConnection.java
+ * ============================================================
+ * Copyright 2013 University of Alberta
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ * ============================================================ 
+ */
 package ca.ualberta.physics.cssdp.file.remote.protocol;
 
 import java.io.File;
@@ -13,7 +31,7 @@ import ca.ualberta.physics.cssdp.domain.file.Host;
 import ca.ualberta.physics.cssdp.domain.file.RemoteFile;
 
 /**
- * A FileSocketConnection does not really involve a socket, but it does act like
+ * A FileConnection does not really involve a socket, but it does act like
  * a SocketConnection: you can use a FileSocketConnection to get a listing of a
  * directory, to copy a file from one place to another, etc, all on some
  * "normal" mounted file system, sans socket.
