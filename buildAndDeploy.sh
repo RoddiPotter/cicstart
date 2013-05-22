fe372565f5e5995ea33b073416d1c5a13811dfe8
Roddi Potter <potter.rod@gmail.com>
2013-05-22 03:48:25 +0000

    updated for building binary macro client via web service

--- buildAndDeploy.sh
+++ buildAndDeploy.sh
@@ -11,4 +11,10 @@ sudo -u jetty cp $GIT_DIR/auth/$LIB_DIR/auth-$VERSION.war $DEPLOY_DIR/auth.war
 sudo -u jetty cp $GIT_DIR/catalogue/$LIB_DIR/catalogue-$VERSION.war $DEPLOY_DIR/catalogue.war
 sudo -u jetty cp $GIT_DIR/file/$LIB_DIR/file-$VERSION.war $DEPLOY_DIR/file.war
 sudo -u jetty cp $GIT_DIR/vfs/$LIB_DIR/vfs-$VERSION.war $DEPLOY_DIR/vfs.war
+sudo -u jetty cp $GIT_DIR/macro/$LIB_DIR/macro-$VERSION.war $DEPLOY_DIR/macro.war
+
+gradle -b $GIT_DIR/macro/build.gradle clean distZip
+unzip $GIT_DIR/macro/build/distributions/macro-$VERSION.zip -d $GIT_DIR/macro/build/distributions
+rm -rf /cicstart/macro/client/macro-$VERSION
+mv -fv $GIT_DIR/macro/build/distributions/macro-$VERSION /cicstart/macro/client
 
