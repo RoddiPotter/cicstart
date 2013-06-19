b306c443248cd4c2b4787b40208b0bdc166d0370
Roddi Potter <potter.rod@gmail.com>
2013-06-19 13:50:12 -0600

    new test for main class

---
+++ macro/src/test/java/ca/ualberta/physics/cicstart/macro/MainClassTest.java
@@ -0,0 +1,29 @@
+package ca.ualberta.physics.cicstart.macro;
+
+import java.io.File;
+import java.nio.charset.Charset;
+
+import org.junit.Assert;
+import org.junit.Test;
+
+import ca.ualberta.physics.cssdp.service.ServiceResponse;
+
+import com.google.common.io.Files;
+
+public class MainClassTest {
+
+	@Test
+	public void testFinishesProperly() throws Exception {
+
+		MainClass main = new MainClass();
+
+		String script = Files.toString(new File(MainClassTest.class
+				.getResource("/test.cml").toURI()), Charset.forName("UTF-8"));
+
+		ServiceResponse<String> sr = main.runCmlScript(script, "token");
+
+		// if it hits this, then it's good.  Hanging is no good.
+		Assert.assertTrue(sr.isRequestOk());
+
+	}
+}
