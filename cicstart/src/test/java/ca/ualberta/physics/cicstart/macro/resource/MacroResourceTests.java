9575d63f17430c693a498801816ad86c48b90fdd
Roddi Potter <potter.rod@gmail.com>
2014-07-27 23:02:45 -0600

    make failing test easier to debug; image name changed on dair

--- cicstart/src/test/java/ca/ualberta/physics/cicstart/macro/resource/MacroResourceTests.java
+++ cicstart/src/test/java/ca/ualberta/physics/cicstart/macro/resource/MacroResourceTests.java
@@ -1,7 +1,9 @@
 package ca.ualberta.physics.cicstart.macro.resource;
 
 import static com.jayway.restassured.RestAssured.expect;
+import static com.jayway.restassured.RestAssured.given;
 
+import org.junit.Assert;
 import org.junit.Test;
 
 import ca.ualberta.physics.cssdp.configuration.ResourceUrls;
@@ -23,14 +25,22 @@ public class MacroResourceTests extends MacroTestsScaffolding {
 		InstanceSpec instanceSpec = new InstanceSpec();
 		instanceSpec.setCloud("DAIR");
 		instanceSpec.setFlavor("m1.tiny");
-		instanceSpec.setImage("Ubuntu 12.04 cloudimg amd64");
+		instanceSpec.setImage("Ubuntu 12.04.4");
 		instanceSpec.setRequestId("TESTJOB");
 
-//		String macroUrl = Common.properties().getString("api.url") + "/macro";
-		Response res = expect().statusCode(200).when().given()
-				.content(instanceSpec).and().contentType(ContentType.JSON)
-				.and().headers("CICSTART.session", sessionToken)
+		// String macroUrl = Common.properties().getString("api.url") +
+		// "/macro";
+		Response res = given().content(instanceSpec).and()
+				.contentType(ContentType.JSON).and()
+				.headers("CICSTART.session", sessionToken)
 				.post(ResourceUrls.MACRO + "/vm");
+
+		if (res.getStatusCode() != 200) {
+			System.out.println(res.asString());
+		}
+
+		Assert.assertEquals("Expecting 200, not " + res.getStatusCode(), 200,
+				res.getStatusCode());
 
 		Instance instance = res.as(Instance.class);
 
@@ -41,7 +51,7 @@ public class MacroResourceTests extends MacroTestsScaffolding {
 				.delete(ResourceUrls.MACRO + "/vm");
 
 		// TODO release floating ips
-		
+
 	}
 
 }
